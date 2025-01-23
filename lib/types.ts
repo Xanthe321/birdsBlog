a306a5e942013f8e20de813dd079f99d5d65c949
Xanthe <104931289+Xanthe321@users.noreply.github.com>
2025-01-23 22:33:46 +0300

    #

---
+++ lib/types.ts
@@ -0,0 +1,30 @@
+export type Category = {
+  id: string
+  name: string
+}
+
+export type Post = {
+  slug: string;
+  title: string;
+  date: string;
+  excerpt: string;
+  image: string;
+  readTime: string;
+  categories: string[];
+  author?: {
+    name: string;
+    avatar: string;
+    role: string;
+  };
+  content?: string;
+}
+
+export const categories: Category[] = [
+  { id: "all", name: "Tüm Yazılar" },
+  { id: "birds-of-prey", name: "Yırtıcı Kuşlar" },
+  { id: "songbirds", name: "Ötücü Kuşlar" },
+  { id: "waterfowl", name: "Su Kuşları" },
+  { id: "hummingbirds", name: "Sinek Kuşları" },
+  { id: "photography", name: "Fotoğrafçılık" },
+  { id: "conservation", name: "Koruma" }
+] 
